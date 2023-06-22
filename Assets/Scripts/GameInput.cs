ecf1639ce87a23161131fc58c0a745fe4db70c80
Buğra Akca <bgrakca@gmail.com>
2023-06-22 20:44:52 +0300

    Update input system implementation

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,29 +1,19 @@
+using System;
 using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    private PlayerInputActions _playerInputActions;
+
+    private void Awake()
+    {
+        _playerInputActions = new PlayerInputActions();
+        _playerInputActions.Player.Enable();
+    }
+
     public Vector2 GetNormalizedMovementVector()
     {
-        Vector2 inputVector = new Vector2();
-        
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y += 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y -= 1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x += 1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x -= 1;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
         return inputVector;
     }
 }
