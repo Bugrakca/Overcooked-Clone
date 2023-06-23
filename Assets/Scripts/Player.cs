76fa1de6e81286227cae7a98d9440ea5ba79995a
Buğra Akca <bgrakca@gmail.com>
2023-06-23 20:27:28 +0300

    Add collision detection with boxcast

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -11,13 +12,37 @@ public class Player : MonoBehaviour
     {
         Vector2 inputVector = gameInput.GetNormalizedMovementVector();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * (moveSpeed * Time.deltaTime);
+
+        float playerRadius = 0.7f;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        
+        bool canMove = !Physics.BoxCast(transform.position, Vector3.one * playerRadius, moveDir, Quaternion.identity, moveDistance);
+        if (!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.BoxCast(transform.position, Vector3.one * playerRadius, moveDirX, Quaternion.identity, moveDistance);
+            
+            if (canMove)
+                moveDir = moveDirX;
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0    , 0, moveDir.z).normalized;
+                canMove = !Physics.BoxCast(transform.position, Vector3.one * playerRadius, moveDirZ, Quaternion.identity, moveDistance);
+                
+                if (canMove)
+                    moveDir = moveDirZ;
+            }
+        }
+
+        if (canMove)
+            transform.position += moveDir * (moveSpeed * Time.deltaTime);
 
         _isWalking = moveDir != Vector3.zero;
         
         float rotateSpeed = 10f;
-
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        
+        if (_isWalking)
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 
     public bool IsWalking()
