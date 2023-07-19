4b692509f6a66b43b661f54dbc26a514d5600102
Buğra Akca <bgrakca@gmail.com>
2023-07-19 21:38:35 +0300

    Add progress ui modes, styles

---
+++ Assets/_Scripts/LookAtCamera.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private enum Mode
+    {
+        LookAt,
+        LookAtInverted,
+        CameraForward,
+        CameraForvardInverted,
+    }
+
+    [SerializeField] private Mode mode;
+    
+    private void LateUpdate()
+    {
+        if (mode is Mode.LookAt)
+        {
+            //Cached by default
+            transform.LookAt(Camera.main.transform);
+        }
+
+        if (mode is Mode.LookAtInverted)
+        {
+            Vector3 dirFromCamera = transform.position - Camera.main.transform.position;
+            transform.LookAt(transform.position + dirFromCamera);
+        }
+        if (mode is Mode.CameraForward)
+        {
+            transform.forward = Camera.main.transform.forward;
+        }
+        if (mode is Mode.CameraForvardInverted)
+        {
+            transform.forward = -Camera.main.transform.forward;
+        }
+        
+    }
+}
